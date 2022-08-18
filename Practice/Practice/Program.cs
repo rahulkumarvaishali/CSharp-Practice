cc4b85254963e79060619487be7f7065e51dfb5a
Rahul kumar <98035034+rahulkumarvaishali@users.noreply.github.com>
2022-08-19 01:51:35 +0530

    Practice

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Practice;
+class Patern
+{
+    static void Main()
+    {
+        for(int i = 10; i < 10; i--)
+        {
+
+            for(int j= 0; j < 10; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();    
+        }  
+    }
+}
